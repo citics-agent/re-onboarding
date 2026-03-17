32545759675f1e5b9c67261dd8016215146aff8c
doitac <doitac@citics.vn>
2026-03-17 11:16:42 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
+/*
 export default defineConfig({
   plugins: [react()],
   base: "/",
@@ -9,5 +10,16 @@ export default defineConfig({
     host: "127.0.0.1",
     port: 8085,
     allowedHosts: ["local-re-onboarding.citics.vn"],
+  },
+});
+*/
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/",
+  server: {
+    host: "10.130.114.32",
+    port: 8085,
+    allowedHosts: ["re-onboard.citics.vn"],
   },
 });
