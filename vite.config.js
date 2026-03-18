b1e5632115a016071c47e27ce2eff50ca65bbe5e
doitac <doitac@citics.vn>
2026-03-18 09:58:36 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-
 // https://vite.dev/config/
 /*
 export default defineConfig({
@@ -14,7 +13,6 @@ export default defineConfig({
   },
 });
 */
-
 
 export default defineConfig({
   plugins: [react()],
